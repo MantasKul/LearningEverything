dd30d7a16b4f6fd2b767387e1d9cf1c9f28b021f
Mantas <m.kulikauskas01@gmail.com>
2023-08-09 17:12:01 +0300

    Added binary search in TDD implementation and test

--- TDD/src/main/java/org/example/Main.java
+++ TDD/src/main/java/org/example/Main.java
@@ -3,10 +3,29 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr = {1, 2, 3, 4, 5};
+        int x = 8;
+        binarySearch(arr, x);
     }
 
     public int compare(int n1, int n2) {
         if (n1 > n2) return 1;
+        return -1;
+    }
+
+    // Return index of the value x in arr, -1 if value doesn't exist
+    public static int binarySearch(int arr[], int x) {
+        int left = 0, right = arr.length - 1;
+
+        while ( left <= right) {
+            int middle = left + (right-left) / 2;
+
+            if(arr[middle] == x) return middle;
+
+            if(arr[middle] < x) left = middle + 1;
+            else right = middle - 1;
+        }
+
         return -1;
     }
 }
--- TDD/src/test/java/MainTest.java
+++ TDD/src/test/java/MainTest.java
@@ -10,4 +10,14 @@ public class MainTest {
         int value = main.compare(2,1);
         assertEquals(1, value);
     }
+
+    // Binary search test
+    @Test
+    public void binarySearch() {
+        Main main = new Main();
+        int arr[] = {2, 3, 4, 5, 10, 10, 15};
+        assertEquals(0, main.binarySearch(arr, 2));
+        assertEquals(-1, main.binarySearch(arr, 6));
+        assertEquals(5, main.binarySearch(arr, 10));
+    }
 }
